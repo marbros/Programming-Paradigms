a598f2b25b31c5f99ba77b84a009e082e1b2b403
MarioG <enigsea@gmail.com>
2016-02-28 21:11:54 -0500

    Some exercises solved of book the craft of FP 2ed

---
+++ Exercises.hs
@@ -0,0 +1,65 @@
+--Exercise 4.1
+
+maxThree :: Int -> Int -> Int -> Int
+maxThree a b c
+    | a >= b && a >= c  = a
+    | b >= c            = b
+    | otherwise         = c
+
+maxFour1 :: Int -> Int -> Int -> Int -> Int
+maxFour1 a b c d 
+    | a >= maxThree b c d  = a
+    | otherwise            = maxThree b c d
+
+    maxFour2 :: Int -> Int -> Int -> Int -> Int
+    maxFour2 a b c d = max (max (max a b) c) d
+
+    maxFour3 :: Int -> Int -> Int -> Int -> Int
+    maxFour3 a b c d = max (maxThree a b c) d
+
+--Exercise 4.5
+
+rangeProduct :: Int -> Int -> Int
+rangeProduct a b
+  | a < b      = a * rangeProduct (a + 1) b
+  | a == b     = a
+  | otherwise  = 0
+
+
+--Exercise 4.6
+
+fac :: Int -> Int
+fac n = rangeProduct 1 n
+
+
+--Exercise 4.7
+
+mult :: Int -> Int -> Int
+mult 0 y = 0
+mult x y = mult (x - 1) y + y
+
+
+--Exercise 4.8
+
+findroot :: Int -> Int -> Int
+findroot n s
+  | s^2 > n    = s - 1
+  | otherwise  = findroot n (s + 1)
+
+intsqrt :: Int -> Int
+intsqrt n = findroot n 1
+
+
+--Exercise 4.13
+
+hcf :: Int -> Int -> Int
+hcf x 0 = x
+hcf x y = hcf y (rem x y)
+
+
+--Exercise 4.14
+
+twon :: Int -> Int
+twon 0 = 1
+twon 1 = 2
+twon n = 2 * (twon (n - 1))
