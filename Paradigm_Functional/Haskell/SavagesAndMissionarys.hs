a6ac054f698934755b9c78eab81bef512ea8aefc
MarioG <enigsea@gmail.com>
2016-04-12 21:39:35 -0500

    the definition of pairs is added

--- Paradigm_Functional/Haskell/SavagesAndMissionarys.hs
+++ Paradigm_Functional/Haskell/SavagesAndMissionarys.hs
@@ -3,6 +3,9 @@ type missionarys   = Int
 type boatPosition  = Int
 data river         = R missionarys boatPosition deriving(Show, Eq)
 
+pairs :: [(missionarys, cannibals)]
+pairs = [(2,0), (1,0), (1,1), (0,1), (0,2)]
+
 noCome :: cannibals -> missionarys -> Bool
 noCome _0  = True
 noCome c m = m >= c
